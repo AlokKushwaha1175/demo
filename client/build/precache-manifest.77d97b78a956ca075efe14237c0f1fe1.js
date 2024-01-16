e42d640f48f211b59a154d81037d7b5aec77ad11
Ubuntu <ubuntu@ip-10-0-28-95.ap-south-1.compute.internal>
2024-01-16 14:45:42 +0000

    Merge changes from remote repository

---
+++ client/build/precache-manifest.77d97b78a956ca075efe14237c0f1fe1.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "10fae1640c3e0d71d0ae7df8ad6869df",
+    "url": "/index.html"
+  },
+  {
+    "revision": "7bdae371c360651ab1d5",
+    "url": "/static/css/main.a1d481cb.chunk.css"
+  },
+  {
+    "revision": "5f69ea4e1595d8d74660",
+    "url": "/static/js/2.4d6c96b3.chunk.js"
+  },
+  {
+    "revision": "90377550dd86ed303dd30d8f487872de",
+    "url": "/static/js/2.4d6c96b3.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "7bdae371c360651ab1d5",
+    "url": "/static/js/main.b2a23a5d.chunk.js"
+  },
+  {
+    "revision": "693d8af27c1684c7fee9",
+    "url": "/static/js/runtime-main.20628b47.js"
+  },
+  {
+    "revision": "5c0c63fb62ec323a10598d7e9de4609e",
+    "url": "/static/media/memories.5c0c63fb.png"
+  }
+]);
